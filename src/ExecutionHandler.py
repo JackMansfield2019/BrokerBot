ae75b019ef397bb9ce69cc19fac1284f7f6fca2b
Andy Pham <70230763+somedude554@users.noreply.github.com>
2021-02-26 16:46:58 -0600

    Drafted Abstract Class

--- src/ExecutionHandler.py
+++ src/ExecutionHandler.py
@@ -1,22 +1,56 @@
 import config
-import requests, json
+import requests
+import json
+from abc import ABC, abstractmethod
 
-class execution_handler(object):
 
-    def __init__(self,API_key_id,API_secret_key,base_url):
+class execution_handler(ABC):
+
+    @abstractmethod
+    def get_account(self):
+        pass
+
+    @abstractmethod
+    def create_order(self):
+        pass
+
+    @abstractmethod
+    def get_orders(self):
+        pass
+
+    @abstractmethod
+    def check_limitations(self):
+        pass
+
+    @abstractmethod
+    def trade_signal(self):
+        pass
+
+    @abstractmethod
+    def purchase_calculation(self):
+        pass
+
+
+class alpaca_execution_handler(execution_handler):
+
+    def __init__(self, API_key_id, API_secret_key, base_url):
         self.base_url = base_url
         self.account_url = "{}/v2/account".format(self.base_url)
         self.order_url = "{}/v2/orders".format(self.base_url)
-        self.headers = {"APCA-API-KEY-ID": API_key_id, "APCA-API-SECRET-KEY": API_secret_key}
-        self.api = tradeapi.REST(headers[APCA-API-KEY-ID],headers[APCA-API-SECRET-KEY],base_url)
+        self.headers = {"APCA-API-KEY-ID": API_key_id,
+                        "APCA-API-SECRET-KEY": API_secret_key}
+        self.api = tradeapi.REST(headers[APCA-API-KEY-ID],
+                                 headers[APCA-API-SECRET-KEY], base_url)
         self.api_account = api.get_account()
 
-    def get_account():
+    def get_account(self):
         return account
 
-    def create_order(self, symbol, qty, side, type, time_in_force, limit_price=None, stop_price=None,
-                    trail_price=None, trail_percent=None, extended_hours=False, client_order_id=None,
-                    order_class=None, take_profit=None, stop_loss=None):
+    def create_order(self, symbol, qty, side, type, time_in_force,
+                     limit_price=None, stop_price=None, trail_price=None,
+                     trail_percent=None, extended_hours=False,
+                     client_order_id=None, order_class=None, take_profit=None,
+                     stop_loss=None):
         data = {
             "symbol": symbol,
             "qty": qty,
@@ -41,15 +75,19 @@ class execution_handler(object):
         r = requests.get(self.order_url, headers=self.headers)
         return json.loads(r.content)
 
-    def check_limitations(self,api_account):
+    def check_limitations(self, api_account):
         """
         Goes through account details and checks if there are any limitations
-        that prevents buying shares. Returns true if no limitations, false otherwise
+        that prevents buying shares. Returns true if no limitations,
+        false otherwise
         """
         return True
 
-    def use_strategy():
+    def trade_signal(self):
         """
         Takes data from stategy handler and buys/sells stocks based on strategy
         """
+        return
+
+    def purchase_calculation(self):
         return
