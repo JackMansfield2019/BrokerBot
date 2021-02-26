779931b8f859a6bb04cba7612e9b5e8b4f511d6b
Will <williamcarter3.14159@gmail.com>
2021-02-26 16:32:31 -0500

    DataHandler v0.1 (w/Bars)
    Added a preliminary abstract class, and cleaned up the outline code for the Alpaca subclass. Also added a very basic implementation of get_bars().
    
    Class needs work, but its a start.

--- src/DataHandler.py
+++ src/DataHandler.py
@@ -1,31 +1,91 @@
-import config
-import websocket, json
+import config, websocket, requests, json
+from abc import ABC, abstractmethod # Abstract class module for python.
+import alpaca_trade_api as tradeapi
 
-class data_handler(object):
+# Abstract base class for the data handler, to facilitate different apis using subclasses.
+class DataHandler(ABC):    
+    @abstractmethod
+    def get_account(self):
+        pass
+    @abstractmethod
+    def set_account(self, account):
+        pass
+
+    def set_socket(self):
+        pass
+
+    def get_socket(self):
+        pass
+        
+    def submit_order(self): 
+        pass
+
+    @abstractmethod
+    def get_bars(self, tickers, bar_timeframe, num_of_bars):
+        pass
+
+    def on_open(self):
+        pass
+
+    def on_message(self):
+        pass
+
+    def on_close(self):
+        pass
+
+    def on_error(self):
+        pass
+
+    def listen(self):
+        pass
+
+    def unlisten(self):
+        pass
+
+
+class AlpacaDataHandler(DataHandler):
     def __init__(self,API_key_id,API_secret_key,base_url,socket = "wss://data.alpaca.markets/stream"):
         self.headers = {"APCA-API-KEY-ID":API_key_id, "APCA-API-SECRET-KEY":API_secret_key}
         self.base_url = base_url
         self.account_url= "{}/v2/account".format(self.base_url)
         self.order_url = "{}/v2/orders".format(self.base_url)
-        self.api = tradeapi.REST(headers[APCA-API-KEY-ID],headers[APCA-API-SECRET-KEY],base_url)
-        self.api_account = api.get_account()
-        self.ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
-        
+        self.api = tradeapi.REST(self.headers["APCA-API-KEY-ID"],self.headers["APCA-API-SECRET-KEY"],base_url)
+        self.api_account = self.api.get_account()
+        self.ws = websocket.WebSocketApp(socket, 
+                    on_message = lambda msg: self.on_message(msg),
+                    on_error   = lambda msg: self.on_error(msg),
+                    on_close   = lambda:     self.on_close(),
+                    on_open    = lambda:     self.on_open())
+    
     def get_account(self):
-        return account
-    def set_account(self,acc):
-        self.acount = acc
+        return self.api_account
+
+    def set_account(self, account):
+        self.acount = account
+
     def set_socket(self,socket = "wss://data.alpaca.markets/stream"):
-        self.ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
-    def get_socket():
+        self.ws = websocket.WebSocketApp(socket, 
+                    on_message = lambda msg: self.on_message(msg),
+                    on_error   = lambda msg: self.on_error(msg),
+                    on_close   = lambda:     self.on_close(),
+                    on_open    = lambda:     self.on_open())
+    def get_socket(self):
         return self.ws
         
     def submit_order(self,symbol, qty, side, type, time_in_force, limit_price=None, stop_price=None, 
                    client_order_id=None, order_class=None, take_profit=None, stop_loss=None, 
                    trail_price=None, trail_percent=None):
-        api.submit_order(symbol, qty, side, type, time_in_force, limit_price, stop_price, 
+        self.api.submit_order(symbol, qty, side, type, time_in_force, limit_price, stop_price, 
                         client_order_id, order_class, take_profit, stop_loss, 
                         trail_price, trail_percent)
+    
+    def get_bars(self, tickers, bar_timeframe, num_of_bars):
+        
+        url = 'https://data.alpaca.markets/v1/bars'+'/'+bar_timeframe+'?symbols='+tickers+'&limit='+num_of_bars
+        r = requests.get(url, headers=self.headers)
+        print(json.dumps(r.json(), indent=4))
+
+    
     def on_open(self):
         """
         function called whenever a websocket is opened, authenticates with alpaca
@@ -45,13 +105,13 @@ class data_handler(object):
         print("closed connection")
 
     def on_error(self, error):
-        print error
+        print(error)
 
     def listen(self,ticker,channel_name):
         """
         function that sends a listen message to alpaca for the streams inputed.
         """
-        for x in range ticker
+        for x in range(ticker):
             ticker[x] = channel_name + ticker[x]
 
         listen_message = {"action": "listen", "data": {"streams": ticker}}
@@ -63,7 +123,7 @@ class data_handler(object):
         function that unlistens for the streams inputed.
             might need error checking if a stream that is not currently being listened to is asked to be unlistened.
         """
-        for x in range ticker
+        for x in range(ticker):
             ticker[x] = channel_name + ticker[x]
         unlisten_message = {"action": "unlisten", "data": {"streams": ticker}}
         self.ws.send(json.dumps(unlisten_message))
