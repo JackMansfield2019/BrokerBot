4475651d2ee9dd2fe481d7a892ad900a2b2d7bb2
Jack Mansfield <mansfj@rpi.edu>
2021-03-23 17:02:59 -0400

    created standard documentation template in BB and started desining a selection system for when creating broker bots

--- src/BrokerBot.py
+++ src/BrokerBot.py
@@ -1,15 +1,42 @@
 # import DataHandler
 # import ExecutionHandler
 # import PortfolioManager
-# import StrategyHandler
+import StrategyHandler
 from DataHandler import AlpacaDataHandler
 from threading import Thread
+from queue import PriorityQueue
+from enum import Enum
+import ENUMS
 import time
 
+"""
+overview: (description of the class)
 
+TODO: (to do of the class as a whole more long term things)
+"""
 class BrokerBot:
-    def __init__(self, api_key, secret_key, base_url, data_url):
 
+    """
+    Abstract Function:
+
+    Representation Invariant:
+
+    Simple explanation:(if nesscary)
+    """
+
+#====================Creators====================
+    '''
+        Overview: returns the previous 5-minute-volume for the given stock by the client 
+
+        Requires: stock is not null
+        Modifies: none
+        Effects: none
+        Returns: volume of the stock that was passed in 
+
+        TODO: How many tickers are we limited to per API request? Answer: 200 
+        sockets limited to 30 
+    '''
+    def __init__(self, api_key, secret_key, base_url, data_url):
         self.headers = {
             "APCA-API-KEY-ID": api_key,
             "APCA-API-SECRET-KEY": secret_key
@@ -25,18 +52,38 @@ class BrokerBot:
 
         self.data_handler = AlpacaDataHandler(self.api_key, self.secret_key,
                                               self.base_url, self.data_url, "ws://127.0.0.1:8765")
+        self.strategy_handlers = []
+        self.strategy_handler.append(self.api_key, self.secret_key, self.base_url, self.data_url,self.order_url)
+        #self.priority_queue = PriorityQueue()
+        
+#====================Observers====================
+    '''
+        Overview: spins up Trades on  
 
-        # self.stream_conn = StreamConn(
-        #     api_key,
-        #     secret_key,
-        #     base_url=URL(self.base_url),
-        #     data_url=URL(self.data_url)
-        # )
+        Requires: none
+        Modifies: none
+        Effects: none
+        Returns: none
+        Throws: RuntimeError on execution
 
+        TODO: Specfification
+    '''
     def get_account(self):
         r = requests.get(self.account_url, headers)
         return json.loads(r.content)
+#====================Producers====================
+#====================Mutators====================
+    '''
+        Overview: Creates & runs a Data handler thread that listens to ticker 
 
+        Requires: Ticker list of strings containg ticker symbols
+        Modifies: Data_handler
+        Effects: Data_handler is initalized and listens to tickers
+        Returns: none
+        Throws: none
+
+        TODO: Specfification
+    '''
     def test_stream_data(self, ticker):
         listening_thread = Thread(target = self.data_handler.start_streaming, args=(ticker,))
         listening_thread.start()
@@ -49,3 +96,20 @@ class BrokerBot:
         # self.data_handler.listen([ticker], "T")
 
         # self.data_handler.listen([ticker], "T")
+
+
+    '''
+        Overview: spins up Trades on  
+
+        Requires: none
+        Modifies: none
+        Effects: none
+        Returns: none
+        Throws: RuntimeError on execution
+
+        TODO: Specfification
+    '''
+    def Run():
+        raise RuntimeError('Failed to open database') from exc
+
+
