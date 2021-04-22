5a0597b6b32ccba94d96deb47c9237e7ea8a4e36
tsanford13 <tommysanford1313@yahoo.com>
2021-04-22 16:07:21 -0400

    adding initial priority queue implementation

--- src/Strategy.py
+++ src/Strategy.py
@@ -7,10 +7,10 @@ import queue
 
 """
 overview:
-    - Strategy Class: Strategy is a base class for all strategies in BrokerBot. 
+    - Strategy Class: Strategy is a base class for all strategies in BrokerBot.
       It takes in a DataHandler and an ExecutionHandler, which it uses to communicate
       with the given broker. (Broker type and trade type are set by the StrategyHandler)
-      It then runs its strategy, which is logged continuously by Cerebro. This allows for 
+      It then runs its strategy, which is logged continuously by Cerebro. This allows for
       backtesting and analysis of the stretegy after the fact.
 
 Requires:   api_key,secret_key,base_url,data_url,socket, base_url, ticker,
@@ -19,10 +19,10 @@ Requires:   api_key,secret_key,base_url,data_url,socket, base_url, ticker,
 Modifies: DH, EH,log,
 Effects: DH is initialized, EH is initialized, stream is listend to, Log is initialized.
 Returns: none
-TODO: 
+TODO:
     - Create more strategies.
     - Refine backtrading setup and functions.
-    -  
+    -
 """
 class Strategy(ABC):
     @abstractmethod
@@ -32,11 +32,11 @@ class Strategy(ABC):
         self.ticker = ticker
         self.dh_queue = None
         self.eh_conn = None
-    
+        self.queue = []
     @abstractmethod
     def start(self):
         
-        #instantiate connections to the data handler and execution handler. 
+        #instantiate connections to the data handler and execution handler.
         st_dh_queue = queue.LifoQueue()
         st_eh_conn, eh_sh_conn = Pipe()
         self.set_eh_dh_conns(st_dh_queue, st_eh_conn)
@@ -57,7 +57,7 @@ class Strategy(ABC):
         #Buy Conditional
 
 
-        # Sell Conditional    
+        # Sell Conditional
         
         pass
    
@@ -75,3 +75,30 @@ class Strategy(ABC):
             raise RuntimeError('set_eh_dh_conns called with a null') from exc
         self.dh_queue = dh_q
         self.eh_conn = eh_conn
+    """
+    Overview: adds a stock to the queue
+
+    Requires: none
+    Modifies: self.queue
+    Effects: appends "stock" to self.queue
+    Returns: none
+    Throws: none
+    """
+    def add_queue(self, stock):
+        printf("Adding {} to queue".format(stock))
+        self.queue.append(stock)
+    """
+    Overview: pops a stock from the queue
+
+    Requires: queue is not empty
+    Modifies: self.queue
+    Effects: appends "stock" to self.queue
+    Returns: none
+    Throws: RuntimeError if queue is empty
+    """
+    def pop_queue(self, pos=0):
+        if len(self.queue) == 0:
+            raise RuntimeError('cannot pop element from an empty queue') from exc
+        else:
+            printf("Popping {} from queue".format(self.queue[pos]))
+            self.queue.pop(pos)
