656f7897e02976d489a4f1355549572f27028ae8
Ethan Tuttle <ethantut@gmail.com>
2021-08-02 17:07:28 -0400

    Added setup for password encryption to database

--- server/server/views.py
+++ server/server/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from passlib.hash import pbkdf2_sha256
 
 @csrf_exempt 
 def login(request):
@@ -12,6 +13,9 @@ def login(request):
             password = data["password"]
             found = False
             #TODO: look for it in the database
+            #As a note, in order to do the encryption you must call pbkdf2_sha256.verify(password, foundPassword)
+            #where foundPassword is the password for the username that you find in the database
+            #No decrption or rehashing, just using the library's verify function
             if (found):
                 return JsonResponse({'username': user, 'isAuthenticated': True})
         return JsonResponse({'isAuthenticated': False})
@@ -24,20 +28,23 @@ def register(request):
             data = json.loads(request.body)
             user = data["username"]
             password = data["password"]
-        found = False
-        added = False
-        #TODO: look for user and try to add
-        if (found):
-            return JsonResponse({'alreadyExists': True, 'isAuthenticated': False})
-        elif (not added):
-            return JsonResponse({'alreadyExists': False, 'isAuthenticated': False})
-        else: #not found and added
-            return JsonResponse({'username': user, 'isAuthenticated': True})
+            found = False
+            added = False
+            hashedPassword = pbkdf2_sha256.hash(password)
+            #TODO: look for user and try to add (use hashedPassword for add instead of plain password)
+            if (found):
+                return JsonResponse({'alreadyExists': True, 'isAuthenticated': False})
+            elif (not added):
+                return JsonResponse({'alreadyExists': False, 'isAuthenticated': False})
+            else: #not found and added
+                return JsonResponse({'username': user, 'isAuthenticated': True})
     else:
         return HttpResponse("404: Route not available")
 
+#TODO: remove or add for debug mode
 def id(request, key_id):
     if request.method == 'GET':
+        #TODO: display user from database/more information or something
         return HttpResponse("Hello I am user {}".format(key_id))
     else:
         return HttpResponse("404: Route not available")
