79a5a66d6283d7a47fa087830a247b3f312f25b4
shanthni <57296126+shanthni@users.noreply.github.com>
2021-08-03 13:59:54 -0400

    Added files to hold all raw sql commands

---
+++ server/database/SQL/DML.sql
@@ -0,0 +1,2 @@
+select * from brokerbot_configuration_users where username = 'username' and password_ = 'password';
+insert into brokerbot_configuration_users (username, password_) values ('username', 'password');
